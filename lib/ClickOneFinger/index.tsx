57b114179ce251db180f5167c7d125e646f614a6
justabayet <anthony.bayet@hotmail.com>
2024-04-25 00:05:23 +0200

    # Import assets from corresponding files

--- lib/ClickOneFinger/index.tsx
+++ lib/ClickOneFinger/index.tsx
@@ -1,13 +1,17 @@
 /**
  * Original icon reference: https://www.flaticon.com/free-animated-icons/click Click animated icons created by Freepik - Flaticon
  */
+import big_dark from './click_touch_big_dark.gif'
+import small_dark from './click_touch_small_dark.gif'
+import big_light from './click_touch_big_light.gif'
+import small_light from './click_touch_small_light.gif'
 import { AnimatedIconProps, VariationList } from '../interface'
 
 const variations: VariationList = {
-  big_dark: 'click_touch_big_dark.gif',
-  small_dark: 'click_touch_small_dark.gif',
-  big_light: 'click_touch_big_light.gif',
-  small_light: 'click_touch_small_light.gif',
+  big_dark,
+  small_dark,
+  big_light,
+  small_light,
 }
 
 function ClickOneFinger({ size = 'big', theme = 'dark', ...props }: AnimatedIconProps): JSX.Element {
