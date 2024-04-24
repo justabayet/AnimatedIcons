120e9028d05c203f5b9af9cbf308bfc9da29eda1
justabayet <anthony.bayet@hotmail.com>
2024-04-25 00:19:51 +0200

    + Improve dev screen

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,49 @@
+import { PropsWithChildren } from "react"
 import { ClickOneFinger, SwipeHorizontal, SwipeVertical } from "../lib/main"
+
+function Row({ children, isDark = false }: PropsWithChildren & { isDark?: boolean }): JSX.Element {
+  return (
+    <div style={{
+      backgroundColor: isDark ? "#FFF" : "#000",
+      display: 'flex',
+      alignItems: 'center',
+      justifyItems: 'center'
+    }}>
+      {children}
+    </div>
+  )
+}
 
 function App(): JSX.Element {
   return (
-    <div>
-      <ClickOneFinger />
-      <SwipeHorizontal />
-      <SwipeVertical />
+    <div id="table">
+      <h1>ClickOneFinger</h1>
+      <Row isDark>
+        <ClickOneFinger />
+        <ClickOneFinger size="small" />
+      </Row>
+      <Row>
+        <ClickOneFinger theme="light" />
+        <ClickOneFinger size="small" theme="light" />
+      </Row>
+      <h1>SwipeHorizontal</h1>
+      <Row isDark>
+        <SwipeHorizontal />
+        <SwipeHorizontal size="small" />
+      </Row>
+      <Row>
+        <SwipeHorizontal theme="light" />
+        <SwipeHorizontal size="small" theme="light" />
+      </Row>
+      <h1>SwipeVertical</h1>
+      <Row isDark>
+        <SwipeVertical />
+        <SwipeVertical size="small" />
+      </Row>
+      <Row>
+        <SwipeVertical theme="light" />
+        <SwipeVertical size="small" theme="light" />
+      </Row>
     </div>
   )
 }
